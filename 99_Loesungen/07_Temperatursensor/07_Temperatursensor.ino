d7313933c4741f1789947c8ce2fe1fc21f0efd2d
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2023-09-20 14:38:55 +0200

    Update 07_Temperatursensor.ino

--- 99_Loesungen/07_Temperatursensor/07_Temperatursensor.ino
+++ 99_Loesungen/07_Temperatursensor/07_Temperatursensor.ino
@@ -1,41 +1,36 @@
-/*
-Der Sensor hat drei Anschlüsse. Beim Blick auf die flache Seite des Sensors:
-- links 5V
-- rechts GND
-- in der Mitte der Pin für das Temperatursignal
+/************************( Importieren der genutzten Bibliotheken )************************/
+#include "DHT.h"    // dht.h importieren falls nicht verfügbar            
+#define DHTPIN 2          // Hier die Pin Nummer eintragen wo der Sensor angeschlossen ist
+#define DHTTYPE DHT11     // Hier wird definiert was für ein Sensor ausgelesen wird. In 
+                          // unserem Beispiel möchten wir einen DHT11 auslesen, falls du 
+                          // ein DHT22 hast einfach DHT22 eintragen
+       
+/********************************( Definieren der Objekte )********************************/                          
+DHT dht(DHTPIN, DHTTYPE); //definiert Sensor - nichts ändern
 
-Auf dem Signalpin gibt der Sensor eine Spannung zwischen 0 und 2,0 Volt aus. Wobei 0V -50 °C entsprechen und der Wert 2,0V entspricht 150 °C. 
-Laut Hersteller ist der Sensor zwischen -40 °C und +125 °C einigermaßen genau (±2 °C). 
-Die Spannung dieses Pins muss vom Mikrocontroller-Board ausgelesen und in einen Temperaturwert umgerechnet werden.
-
-ACHTUNG: Wenn der Sensor falsch angeschlossen wird, brennt er durch!
-
-Der „map“ Befehl wird nun verwendet, um diesen Zahlenwert zwischen 0 und 410 wieder in einen Temperaturwert zwischen -50 °C und +150 °C umzuwandeln.
-temperatur = map(sensorwert, 0, 410, -50, 150);
-temperatur = map (a , b , c , d , e)
-a= umzuwandelnde Zahl
-b= minimum Messbereich
-c= maximum Messbereich
-d= minimum Ausgabewert
-e= maximum Ausgabewert
-*/
-
-
-int TMP = A0; //TMP steht für den PIN an welchem der Temperatur-Sensor angeschlossen ist
-int sensorwert;
-int temperatur = 0; //Unter der Variablen "temperatur" wird später der Temperaturwert abgespeichert.
-int t = 500; //Der Wert für „t“ gibt im Code die zeitlichen Abstände zwischen den einzelnen Messungen vor.
-
-void setup() 
-{
-Serial.begin(9600); // Verbinden mit dem Serial Monitor
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 Testprogramm");
+  dht.begin();
 }
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);                     // Hier definieren wir die Verweilzeit die gewartet wird  
+                                   // bis der Sensor wieder ausgelesen wird. Da der DHT11  
+                                   // auch ca. 2 Sekunden hat um seine Werte zuaktualisieren
+                                   // macht es keinen sinn ihn schneller auszulesen!
+                                    
+  float h = dht.readHumidity();    // Lesen der Luftfeuchtigkeit und speichern in die Variable h
+  float t = dht.readTemperature(); // Lesen der Temperatur in °C und speichern in die Variable t
+  
 
-void loop() 
-{
-sensorwert=analogRead(TMP); //Auslesen des Sensorwertes.
-temperatur= map(sensorwert, 0, 410, -50, 150); //Umwandeln des Sensorwertes mit Hilfe des "map" Befehls.
-delay(t); // Nach jeder Messung ist je eine kleine Pause mit der Dauer „t“ in Millisekunden.
-Serial.print(temperatur); //Nun wird der Wert „temperatur“ über die serielle Kommunikation an den PC gesendet. Durch öffnen des seriellen Monitors in der Arduino-Software kann die Temperatur abgelesen werden.
-Serial.println(" Grad Celsius"); // Im seriellen Monitor wird hinter der Temperatur die Einheit eingeblendet.
+  // Nun senden wir die gemessenen Werte an den PC dise werden wir im Seriellem Monitor sehen
+  Serial.print("Luftfeuchtigkeit: ");
+  Serial.print(h);                  // Ausgeben der Luftfeuchtigkeit
+  Serial.print("%\t");              // /t ist ein Tabulator = grösserer Abstand im Text
+  Serial.print("Temperatur: ");
+  Serial.print(t);                  // Ausgeben der Temperatur
+  Serial.print("°");                // Schreiben des ° Zeichen
+  Serial.println("C");
+
 }
