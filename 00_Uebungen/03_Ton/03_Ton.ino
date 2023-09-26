780e904f33f4c8d27e31a2ee62ea5a550dad63f9
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2023-09-26 10:37:27 +0200

    Update 03_Ton.ino

--- 00_Uebungen/03_Ton/03_Ton.ino
+++ 00_Uebungen/03_Ton/03_Ton.ino
@@ -1,25 +1,39 @@
+/* Befehle:
+pinMode(PIN, OUTPUT oder INPUT); <- definiert den PIN, OUTPUT gibt Strom und INPUT misst den Strom am PIN
+digitalWrite (PIN, HIGH oder LOW); <- schaltet eine etwas EIN (HIGH) oder AUS (LOW)
+delay(ZEIT); <- macht eine Pause mit der ZEIT in millisekunden
+*/
+
 //Die Erzeugung des Tones basiert maßgeblich auf dem Befehl „tone(x, y)“, 
 //wobei der x-Wert den Pin angibt, an dem der Lautsprecher mit der positiven Seite angeschlossen ist und der y-Wert, der die Tonhöhe angibt.
 
+/*-> NEU: Wir lernen Variabeln! Eine Variabel ist ein Wort welche wir anstelle einer Zahl verwenden können
+mit int sagen wir dem Arduino, dass es sich um eine Zahl handelt. int Taster = 6; bedeutet wir geben dem Wort Taster die Zahl 6.
+Jedes Mal wenn wir das Wort Taster nutzen, dann nimmt das Arduino die Zahl 6. In unserem Fall ist die Zahl 6 der PIN an welchem
+der Taster angeschlossen ist.
+*/
+
 //Definiere zuerst die Variabeln
-int Taster=6;
-int Tasterstatus=0;
-int Piezo = 8;
-int Spielzeit = 1000;
+int Taster = PIN ; //PIN an welchem der Taster angeschlossen ist - muss mit Zahl ergänzt werden
+int Piezo = PIN; //PIN an welchem der Piezo angeschlossen ist - muss mit Zahl ergänzt werden
+int Tasterstatus=0; // nicht ändern. Dies ist ein Startwert und wird definiert damit das Arduino weiss, dass es sich im eine Zahl handelt.
+int Spielzeit = MILLISEKUNDEN; //hier definieren wir die Zeit im Befehl delay(ZEIT) - den kennst du noch von der LED Übung! 
 
 void setup()
 {
-pinMode(Taster, INPUT);
-pinMode(Piezo, OUTPUT);
+pinMode(Taster, INPUT); // Hier nutzen wir das Wort Taster als PIN-Nummer welche oben definiert ist in den Variabeln 
+  // und der Taster in ein Input, da wir den Strom messen am PIN
+pinMode(XXXX, mode); // Nutze hier die Variabel für den Piezo und der Piezo gibt einen Ton aus. Ist es ein INPUT oder OUTPUT?
 }
 
 void loop()
 {
-Tasterstatus = digitalRead(Taster);
-if (Tasterstatus == HIGH) // Wenn der Taster gedrückt ist...
+Tasterstatus = digitalRead(Taster); // hier lesen wir den PIN aus mit digitalRead(PIN) und speichern den Wert in Tasterstatus
+// NEU!! Wir lernen if/else - wenn das passiert mache das (if) und anonsten das andere (else)
+if (Tasterstatus == HIGH) // Wenn der Taster gedrückt ist... also wenn der PIN Strom bekommt, dann ist das Signal HIGH
 {
 tone(Piezo, 300); // …spiele diesen Ton...
-delay(Spielzeit); //…und zwar so lange, wie die Spielzeit definiert ist...
+delay(XXXX); //…und zwar so lange, wie die Spielzeit definiert ist... füge hier die Variabel für die Zeit hinzu
 noTone(Piezo); // Ton abschalten
 }
 else {
