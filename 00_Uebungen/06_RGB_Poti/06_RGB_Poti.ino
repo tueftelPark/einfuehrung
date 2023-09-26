4aba9a4a943bc0b289dffb0afc1b9f6f021f8ad1
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2023-09-26 10:53:00 +0200

    Update 06_RGB_Poti.ino

--- 00_Uebungen/06_RGB_Poti/06_RGB_Poti.ino
+++ 00_Uebungen/06_RGB_Poti/06_RGB_Poti.ino
@@ -1,3 +1,13 @@
+/* Befehle:
+pinMode(PIN, OUTPUT oder INPUT); <- definiert den PIN, OUTPUT gibt Strom und INPUT misst den Strom am PIN
+digitalWrite(PIN, HIGH oder LOW); <- schaltet eine etwas EIN (HIGH) oder AUS (LOW)
+digitalRead(PIN); <- liest den PIN aus. Falls Strom ankommt ist es HIGH und kein Strom LOW
+WERT = digitalRead(PIN); <- das HIGH oder LOW welches gemessen wurde, speichern wir in der Variable WERT (Achtung WERT muss bei den Variabel noch definiert werden!)
+delay(ZEIT); <- macht eine Pause mit der ZEIT in millisekunden
+analogWrite(PIN, 0 bis 255); <- neben digital gibt es auch analoge Signale. Hier gibt es nicht nur HIGH oder LOW, sondern du kannst eine Zahl von 0 bis 255 einsetzen.
+*/
+
+
 /*Ein Drehregler hat drei Anschlüsse. Außen wird + und – angeschlossen. Von dem mittleren Pin geht ein Kabel zu einem analogen Eingangspin am Mikrocontroller-Board. 
 Wenn man den Drehregler dreht, dann gibt der mittlere Pin eine Spannung zwischen 0 und 5 Volt aus. 
 Drehregler ganz links: 0 V und Drehregler ganz rechts: 5V, bzw. Seitenverkehrt, je nach Verkabelung.
@@ -11,22 +21,19 @@ int potiRot= A1; //Das Wort „eingang“ steht jetzt für den PIN an welchem de
 int potiGruen= A2; //Das Wort „eingang“ steht jetzt für den PIN an welchem der Drehregler angeschlossen ist.
 
 int LEDblau = 3; // Farbe blau an Pin 3 -> Wichtig nur PWM(~) nutzen!
-int LEDrot = 5; // Farbe rot an Pin 5 -> Wichtig nur PWM(~) nutzen!
-int LEDgruen = 6; // Farbe gruen an Pin 6 -> Wichtig nur PWM(~) nutzen!
+// Ergänze noch rot und grün
 
 int sensorwertBlau = 0; //Variable für den Sensorwert mit 0 als Startwert
-int sensorwertRot = 0; //Variable für den Sensorwert mit 0 als Startwert
-int sensorwertGruen = 0; //Variable für den Sensorwert mit 0 als Startwert
+// Ergänze noch rot und grün
 
 int helligkeitBlau = 0; //Variable für die Helligkeit mit 0 als Startwert
-int helligkeitRot = 0; //Variable für die Helligkeit mit 0 als Startwert
-int helligkeitGruen = 0; //Variable für die Helligkeit mit 0 als Startwert
+// Ergänze noch rot und grün
 
 void setup()
 {
-pinMode(LEDblau, OUTPUT);
-pinMode(LEDrot, OUTPUT);
-pinMode(LEDgruen, OUTPUT);
+pinMode(LEDblau, mode); // TODO: ist es ein OUTPUT oder INPUT?
+// Ergänze noch rot und grün
+  
 }
 
 void loop()
@@ -35,11 +42,6 @@ void loop()
   helligkeitBlau = sensorwertBlau/4; //1023 / 4 = 255 --> analogWrite geht nur bis 255 und Poti bis 1023
   analogWrite(LEDblau, helligkeitBlau); 
 
-  sensorwertRot = analogRead(potiRot);
-  helligkeitRot = sensorwertRot/4; //1023 / 4 = 255 --> analogWrite geht nur bis 255 und Poti bis 1023
-  analogWrite(LEDrot, helligkeitRot); 
-
-  sensorwertGruen = analogRead(potiGruen);
-  helligkeitGruen = sensorwertGruen/4; //1023 / 4 = 255 --> analogWrite geht nur bis 255 und Poti bis 1023
-  analogWrite(LEDgruen, helligkeitGruen); 
+  // mach das ganze noch für ROT und GRÜN
+  
 }
