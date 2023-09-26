5e00df1bf6a94e1f9e414647a88d9c75803e3737
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2023-09-26 10:57:17 +0200

    Update 07_Fotowiderstand.ino

--- 00_Uebungen/07_Fotowiderstand/07_Fotowiderstand.ino
+++ 00_Uebungen/07_Fotowiderstand/07_Fotowiderstand.ino
@@ -1,17 +1,28 @@
+/* Befehle:
+pinMode(PIN, OUTPUT oder INPUT); <- definiert den PIN, OUTPUT gibt Strom und INPUT misst den Strom am PIN
+digitalWrite(PIN, HIGH oder LOW); <- schaltet eine etwas EIN (HIGH) oder AUS (LOW)
+digitalRead(PIN); <- liest den PIN aus. Falls Strom ankommt ist es HIGH und kein Strom LOW
+WERT = digitalRead(PIN); <- das HIGH oder LOW welches gemessen wurde, speichern wir in der Variable WERT (Achtung WERT muss bei den Variabel noch definiert werden!)
+delay(ZEIT); <- macht eine Pause mit der ZEIT in millisekunden
+analogWrite(PIN, 0 bis 255); <- neben digital gibt es auch analoge Signale. Hier gibt es nicht nur HIGH oder LOW, sondern du kannst eine Zahl von 0 bis 255 einsetzen.
+*/
+
 /*Das Mikrocontroller-Board ist in der Lage, analoge Signale (Spannung) zu messen und diese zu verarbeiten. 
 Dies geschieht mit den analogen Eingängen auf dem Board. Dieser wandelt den gemessenen Spannungswert in eine Zahl um, die dann weiter verarbeitet werden kann. 
 0 Volt entspricht dabei der Zahl 0 und der höchste Messwert 5 Volt entspricht der Zahl 1023 (0 bis 1023 entspricht 1024 Zahlen = 10 Bit).
 */
 
-int eingang= A0; //Das Wort „eingang“ steht  für den PIN an welchem der Sensor angeschlossen ist
-int LED = 10; //Das Wort „LED“ steht für den PIN an welchem die LED angeschlossen ist
+int eingang = A0; //Das Wort „eingang“ steht  für den PIN an welchem der Sensor angeschlossen ist
+int LED = XX; // TODO: Das Wort „LED“ steht für den PIN an welchem die LED angeschlossen ist
 int sensorWert = 0; //Variable für den Sensorwert mit 0 als Startwert
 
 void setup()
 {
+  //-> NEU! Serial-Monitor: Das Arduino kann dir auch Informationen anzeigen lassen, welche du ausliest. Klicke dafür oben rechts auf die Lupe
 Serial.begin(9600); //Die Kommunikation mit dem seriellen Port wird gestartet. Das benötigt man, um sich den tatsächlich ausgelesenen Wert später im serial monitor anzeigen zu lassen.
-pinMode (LED, OUTPUT); //Der Pin mit der LED (Pin XX) ist jetzt ein Ausgang
-//Der analoge Pin muss nicht definiert werden.
+
+//Definiere hier noch die LED
+
 }
 
 void loop()
@@ -21,15 +32,18 @@ Serial.print("Sensorwert = " ); //Ausgabe am Serial-Monitor: Das Wort „Sensorw
 Serial.println(sensorWert); //Ausgabe am Serial-Monitor. Mit dem Befehl Serial.print wird der Sensorwert des Fotowiderstandes in Form einer Zahl zwischen 0 und 1023 an den serial monitor gesendet.
 //ln bedeutet Neue Linie - nach diesem Wert wird in eine neue Zeile geschrieben.
 
-if (sensorWert > 512 ) //Wenn der Sensorwert über 512 beträgt….
+if (sensorWert > 512 ) //Wenn der Sensorwert über 512 beträgt…. 
+  // Starte mal das Programm und schaue, wie sich der Wert verändert, wenn du deine Hand über den Sensor haltest. 
+  // Passe anschliessend den Wert im if-Statement an
 {
-digitalWrite(LED, HIGH); //…soll die LED leuchten…
+// was soll passieren, wenn dieser Wert erreicht ist? 
+  // Schalte mal eine LED an
 }
 
 else //andernfalls…
 {
-digitalWrite(LED, LOW); //….soll sie nicht leuchten.
+// und wenn der Wert nicht erreicht wird? Dann schalte die LED aus
 }
-delay (50);//Eine kurze Pause, in der die LED an oder aus ist
+//Eine kurze Pause, in der die LED an oder aus ist
 }
 
