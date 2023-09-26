93b8c643a1a280ca68f2e4a7e20327fd2f6deb7a
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2023-09-26 11:02:32 +0200

    Update 09_Temperatursensor.ino

--- 00_Uebungen/09_Temperatursensor/09_Temperatursensor.ino
+++ 00_Uebungen/09_Temperatursensor/09_Temperatursensor.ino
@@ -1,10 +1,23 @@
+/* Befehle:
+pinMode(PIN, OUTPUT oder INPUT); <- definiert den PIN, OUTPUT gibt Strom und INPUT misst den Strom am PIN
+digitalWrite(PIN, HIGH oder LOW); <- schaltet eine etwas EIN (HIGH) oder AUS (LOW)
+digitalRead(PIN); <- liest den PIN aus. Falls Strom ankommt ist es HIGH und kein Strom LOW
+WERT = digitalRead(PIN); <- das HIGH oder LOW welches gemessen wurde, speichern wir in der Variable WERT (Achtung WERT muss bei den Variabel noch definiert werden!)
+delay(ZEIT); <- macht eine Pause mit der ZEIT in millisekunden
+analogWrite(PIN, 0 bis 255); <- neben digital gibt es auch analoge Signale. Hier gibt es nicht nur HIGH oder LOW, sondern du kannst eine Zahl von 0 bis 255 einsetzen.
+*/
+
+// ->> Wichtig! Du musst nichts änderen am Programm. Du kannst es durchlesen und versuchen zu verstehen.
+// Öffne den Serial Monitor oben rechts bei der Lupe und schaue die Werte an, welche der Sensor ausgibt.
+// Wenn wir Sensoren verwenden, dann werden die Programme oft vorgegben und wir können diese kostenlos nutzen, um den Sensor anzusteuern.
+// Bei der nächsten Übung werden wir die Temperatur nutzen, um ein Objekt anzusteuern
+
 /************************( Importieren der genutzten Bibliotheken )************************/
 #include "DHT.h"    // dht.h importieren falls nicht verfügbar            
 #define DHTPIN 2          // Hier die Pin Nummer eintragen wo der Sensor angeschlossen ist
 #define DHTTYPE DHT11     // Hier wird definiert was für ein Sensor ausgelesen wird. In 
-                          // unserem Beispiel möchten wir einen DHT11 auslesen, falls du 
-                          // ein DHT22 hast einfach DHT22 eintragen
-       
+                          // unserem Beispiel möchten wir einen DHT11 auslesen
+                                 
 /********************************( Definieren der Objekte )********************************/                          
 DHT dht(DHTPIN, DHTTYPE); //definiert Sensor - nichts ändern
 
