ad08f97ceacdb2f254e1262cdd3a9afe286ac063
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2024-02-26 10:46:02 +0100

    Update 03_Ton.ino

--- 00_Uebungen/03_Ton/03_Ton.ino
+++ 00_Uebungen/03_Ton/03_Ton.ino
@@ -6,7 +6,7 @@ WERT = digitalRead(PIN); <- das HIGH oder LOW welches gemessen wurde, speichern 
 delay(ZEIT); <- macht eine Pause mit der ZEIT in millisekunden
 */
 
-//Die Erzeugung des Tones basiert maßgeblich auf dem Befehl „tone(x, y)“, 
+//Die Erzeugung des Tones basiert massgeblich auf dem Befehl „tone(x, y)“, 
 //wobei der x-Wert den Pin angibt, an dem der Lautsprecher mit der positiven Seite angeschlossen ist und der y-Wert, der die Tonhöhe angibt.
 
 /*-> NEU: Wir lernen Variabeln! Eine Variabel ist ein Wort welche wir anstelle einer Zahl verwenden können
@@ -16,30 +16,43 @@ der Taster angeschlossen ist.
 */
 
 //Definiere zuerst die Variabeln
-int Taster = PIN ; //TODO: PIN an welchem der Taster angeschlossen ist - muss mit Zahl ergänzt werden
-int Piezo = PIN; //TODO: PIN an welchem der Piezo angeschlossen ist - muss mit Zahl ergänzt werden
+//TODO: PIN an welchem der Taster (kleiner Druckknopf) angeschlossen ist - muss mit Zahl ergänzt werden
+int Taster = XX ; 
+//TODO: PIN an welchem der Piezo angeschlossen ist - muss mit Zahl ergänzt werden - der Piezo ist der kleine schwarze "Lautsprecher"
+int Piezo = XX; 
+//TODO: Millisekunden anpassen. hier definieren wir die Zeit im Befehl delay(ZEIT) - den kennst du noch von der LED Übung! 
+int Spielzeit = XX; 
+
 int Tasterstatus=0; // nicht ändern. Dies ist ein Startwert und wird definiert damit das Arduino weiss, dass es sich im eine Zahl handelt.
-int Spielzeit = MILLISEKUNDEN; //TODO: Millisekunden anpassen. hier definieren wir die Zeit im Befehl delay(ZEIT) - den kennst du noch von der LED Übung! 
+
 
 void setup()
 {
+// TODO: Nutze hier die Variabel für den Piezo und der Piezo gibt einen Ton aus. Ist es ein INPUT oder OUTPUT?
+pinMode(XX, YY); 
+
 pinMode(Taster, INPUT); // Hier nutzen wir das Wort Taster als PIN-Nummer welche oben definiert ist in den Variabeln 
-  // und der Taster in ein Input, da wir den Strom messen am PIN
-pinMode(XXXX, mode); // TODO: Nutze hier die Variabel für den Piezo und der Piezo gibt einen Ton aus. Ist es ein INPUT oder OUTPUT?
+// und der Taster in ein Input, da wir den Strom messen am PIN
 }
 
 void loop()
 {
 Tasterstatus = digitalRead(Taster); // hier lesen wir den PIN aus mit digitalRead(PIN) und speichern den Wert in Tasterstatus
-// NEU!! Wir lernen if/else - wenn das passiert mache das (if) und anonsten das andere (else)
+//NEU: Wir lernen if/else - wenn das passiert mache das (if) und anonsten das andere (else)
 if (Tasterstatus == HIGH) // Wenn der Taster gedrückt ist... also wenn der PIN Strom bekommt, dann ist das Signal HIGH
 {
-tone(Piezo, 300); // …spiele diesen Ton...
-delay(XXXX); //TODO: …und zwar so lange, wie die Spielzeit definiert ist... füge hier die Variabel für die Zeit hinzu
+tone(Piezo, 300); // …spiele diesen Ton... //TEST: Verändere die Zahl in diesem Befehel - was passiert?
+  
+//TODO: füge hier die Variabel für die Zeit hinzu - siehe oben bei Definiere die Variabeln wie diese heisst 
+delay(ZZ); 
+
 noTone(Piezo); // Ton abschalten
 }
 else {
   // wenn nichts gedrückt wird, mache nichts
+  // hier musst du nichts ergänzen
+  //TEST: Falls du möchtest kannst du hier etwas programmieren - was soll passieren wenn der Taster nicht gedrückt wird? 
+  //TEST: Leuchtet eine LED? Du darfst dein Wissen von Übung 1 und 2 auch hier anwenden
 }
 }
 
