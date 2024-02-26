01fd64edaaad6defb6e2fc42da7b9b6daaf676e7
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2024-02-26 10:34:00 +0100

    Update 01_LED.ino

--- 00_Uebungen/01_LED/01_LED.ino
+++ 00_Uebungen/01_LED/01_LED.ino
@@ -1,13 +1,14 @@
 /* Befehle:
-pinMode(PIN, OUTPUT oder INPUT); <- definiert den PIN, OUTPUT gibt Strom und INPUT misst den Strom am PIN
-digitalWrite (PIN, HIGH oder LOW); <- schaltet eine etwas EIN (HIGH) oder AUS (LOW)
+pinMode(PIN, OUTPUT oder INPUT); <- definiert den PIN, OUTPUT gibt Strom und INPUT misst den Strom am PIN, PIN mit einer Zahl ersetzen
+digitalWrite (PIN, HIGH oder LOW); <- schaltet etwas EIN (HIGH) oder AUS (LOW) - PIN mit einer Zahl ersetzen, dann HIGH oder LOW
 delay(ZEIT); <- macht eine Pause mit der ZEIT in millisekunden
 */
 
 void setup() // Hier beginnt das Setup
 { 
 
-pinMode(PIN, mode); //TODO: Pin XX wird als Ausgang definiert
+//TODO: Pin XX wird als Ausgang definiert - PIN mit einer Zahl ersetzen und mode durch OUTPUT oder INPUT ersetzen
+pinMode(PIN, mode); 
 
 } // Hier endet ein Programmabschnitt
 
@@ -15,11 +16,11 @@ void loop() // Hier beginnt das Hauptprogramm
 { 
 //TODO: schalte eine LED ein
 
-//TODO: warte 1 Sekunde
+//TODO: warte 1 Sekunde //TEST: Verändere die Zeit - was passiert?
 
 //TODO: schalte die LED wieder aus
 
-//TODO: warte 1 Sekunde
+//TODO: warte 1 Sekunde //TEST: Verändere die Zeit - was passiert?
   
   
 } // Hier endet ein Programmabschnitt
