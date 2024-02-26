13cd6b25d0f5a9df9727d15760145c5183d875af
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2024-02-26 10:49:47 +0100

    Update 04_RGB_LED.ino

--- 00_Uebungen/04_RGB_LED/04_RGB_LED.ino
+++ 00_Uebungen/04_RGB_LED/04_RGB_LED.ino
@@ -4,17 +4,20 @@ digitalWrite(PIN, HIGH oder LOW); <- schaltet eine etwas EIN (HIGH) oder AUS (LO
 digitalRead(PIN); <- liest den PIN aus. Falls Strom ankommt ist es HIGH und kein Strom LOW
 WERT = digitalRead(PIN); <- das HIGH oder LOW welches gemessen wurde, speichern wir in der Variable WERT (Achtung WERT muss bei den Variabel noch definiert werden!)
 delay(ZEIT); <- macht eine Pause mit der ZEIT in millisekunden
-analogWrite(PIN, 0 bis 255); <- neben digital gibt es auch analoge Signale. Hier gibt es nicht nur HIGH oder LOW, sondern du kannst eine Zahl von 0 bis 255 einsetzen.
+analogWrite(PIN, 0 bis 255); <- NEU: neben digital gibt es auch analoge Signale. Hier gibt es nicht nur HIGH oder LOW, sondern eine Zahl von 0 bis 255.
 */
 
 //Zuerst die Variabeln definieren
-int LEDblau = PIN; // TODO: Farbe blau an Pin XX -> Wichtig nur PWM(~) nutzen!
-int LEDrot = PIN; // TODO: Farbe rot an Pin XXX -> Wichtig nur PWM(~) nutzen!
-int LEDgruen = PIN; // TODO: Farbe gruen an Pin XX -> Wichtig nur PWM(~) nutzen!
+// TODO: Farbe blau an Pin XX -> Wichtig nur PWM(~) nutzen! Auf deinem Arduino sind dies die Nummern mit einer kleinen Welle
+int LEDblau = XX; 
+// TODO: Farbe rot an Pin XXX -> Wichtig nur PWM(~) nutzen!
+int LEDrot = XX; 
+// TODO: Farbe gruen an Pin XX -> Wichtig nur PWM(~) nutzen! 
+int LEDgruen = XX; 
 
-int helligkeitBlau = 255; //TESTEN: Variable für die Helligkeit  > eine Zahl zwischen 0 (aus) und 255 (ganz hell)
-int helligkeitRot = 0; //TESTEN: Variable für die Helligkeit  > eine Zahl zwischen 0 (aus) und 255 (ganz hell)
-int helligkeitGruen = 0; //TESTEN: Variable für die Helligkeit  > eine Zahl zwischen 0 (aus) und 255 (ganz hell)
+int helligkeitBlau = 255; //TEST: Variable für die Helligkeit  > eine Zahl zwischen 0 (aus) und 255 (ganz hell)
+int helligkeitRot = 0; //TEST: Variable für die Helligkeit  > eine Zahl zwischen 0 (aus) und 255 (ganz hell)
+int helligkeitGruen = 0; //TEST: Variable für die Helligkeit  > eine Zahl zwischen 0 (aus) und 255 (ganz hell)
 
 void setup()
 {
@@ -31,9 +34,7 @@ void loop()
   analogWrite(LEDblau, helligkeitBlau); //Jetzt wird der PIN für die blaue LED genommen und die Helligkeit von der Variabel
   //TODO: füge hier noch eine Wartezeit von 1sek hinzu
   
-  analogWrite(LEDblau, 25); // TEST: Du kannst auch direkt eine Nummer eingeben um die blaue LED in der Helligkeit zu wechseln
-  
   // TODO: lasse alle anderen RGB-Farben auch leuchten
   // TODO: du kannst sie nacheinander leuchten lassen oder alle gleichzeitig 
-  // TODO: was passiert wenn du Rot, Grün und Blau mischt? 
+  // TEST: was passiert wenn du Rot, Grün und Blau mischt? 
 }
