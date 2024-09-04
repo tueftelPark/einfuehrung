c8818a6fdc8857db105ab80c69fa51969d2595d7
BedaZu <167321432+BedaZu@users.noreply.github.com>
2024-09-04 03:06:45 -0600

    Update 09_OLED_Poti.ino

--- 00_Uebungen/09_OLED_Poti/09_OLED_Poti.ino
+++ 00_Uebungen/09_OLED_Poti/09_OLED_Poti.ino
@@ -1,5 +1,5 @@
 /* Befehle:
-delay(ZEIT); <- macht eine Pause mit der ZEIT in millisekunden
+delay(ZEIT); <- macht eine Pause mit der ZEIT in Millisekunden
 ZAHL = analogRead(PIN); <- Wert zwischen 0 und 1023 wird am PIN gemessen. Dieser Wert wird unter der Variable "ZAHL" gespeichert.
 map(WERT, VON_MIN, VON_MAX, NACH_MIN, NACH_MAX); <- ändert den Bereich eines Wertes von einem Bereich auf einen anderen 
 -> WERT ist der zu ändernde Wert, VON_MIN und VON_MAX sind der alte Bereich, NACH_MIN und NACH_MAX sind der neue Bereich
@@ -27,17 +27,17 @@ Oled.setFlipMode(true); // Legt die Orientation vom Diasplay fest
 
 void loop()
 {
-  // TODO: ersetze X mit der nummer für den Drehregler
-  int sensorWert = analogRead(AX);
+  // TODO: ersetze ? mit der nummer für den Drehregler
+  int Sensorwert = analogRead(A?);
 
   
-  int prozent = sensorWert/10.23; //Der sensorWert wird in eine Zahl zwischen 0-100 umgerechnet
+  int Prozent = Sensorwert/10.23; //Der Sensorwert wird in eine Zahl zwischen 0-100 umgerechnet
   Oled.setFont(u8x8_font_chroma48medium8_r); // Schriftart
   Oled.setCursor(33, 33);    // Position des Textes auf dem Display
   Oled.print("Prozent:");   
-  Oled.print(prozent);  // Der Wert welcher unter der Variabel gespeichert ist wird auf dem Display angezeigt.
+  Oled.print(Prozent);  // Der Wert welcher unter der Variabel gespeichert ist wird auf dem Display angezeigt.
   Oled.println("%"); 
   Oled.refreshDisplay();  // Aktualisiert das Diaplay
   delay(10); // kurze Wartezeit für ein flüssiges Bild auf dem Display
   
-} /
+}
