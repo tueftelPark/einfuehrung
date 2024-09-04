3730c4f4774a0f5e4a283743cec1fb77a9e52d72
BedaZu <167321432+BedaZu@users.noreply.github.com>
2024-09-04 02:31:52 -0600

    Update 06_Dimmer.ino

--- 00_Uebungen/06_Dimmer/06_Dimmer.ino
+++ 00_Uebungen/06_Dimmer/06_Dimmer.ino
@@ -1,42 +1,42 @@
 /* Befehle:
-pinMode(PIN, OUTPUT oder INPUT); <- definiert den PIN, OUTPUT gibt Strom und INPUT misst den Strom am PIN
-delay(ZEIT); <- macht eine Pause mit der ZEIT in millisekunden
-analogWrite(PIN, 0 bis 255); <- NEU: neben digital gibt es auch analoge Signale. Hier gibt es nicht nur HIGH oder LOW, sondern eine Zahl von 0 bis 255.
+pinMode(PIN, OUTPUT oder INPUT); <- definiert den PIN(wo ist das Objekt angeschlossen), PIN mit der Zahl auf dem SensorKit ersetzen (ohne D), INPUT(Sensor/Signaleingang) OUTPUT(Aktor/Signalausgang)
+delay(ZEIT); <- macht eine Pause mit der ZEIT in Millisekunden
+analogWrite(PIN, 0 bis 255); <- NEU: neben digitalen gibt es auch analoge Signale. Hier gibt es nicht nur HIGH oder LOW, sondern jede Zahl zwischen 0 und 255 ist möglich.
 ZAHL = analogRead(PIN); <- Wert zwischen 0 und 1023 wird am PIN gemessen. Dieser Wert wird unter der Variable "ZAHL" gespeichert.
 */
 
-  int helligkeit = 0;
+  int Helligkeit = 0;
   
-  int sensorWert = 0;
-  //TODO: Definiere Poti mit der ensprechenden AX Nummer auf dem Sensor Kit. Ersetze X mit einer Zahl (A inkulsive -> z.B. A7)
-  int Poti = AX;
+  int Sensorwert = 0;
+  //TODO: Definiere RotaryPotentiometer mit der ensprechenden A? Nummer auf dem Sensor Kit. Ersetze ? mit einer Zahl (A inkulsive -> z.B. A7)
+  int RotaryPotentiometer = A?;
 
-  //TODO: Defiere die Variabeln Rot, Blau und Gruen mit den jeweiligen PIN-Nummern. Erestze XX mit der richtigen Zahl (Du musst die PINs 9, 10 ,11 und GND wählen)
-  int Rot = XX;
+  //TODO: Defiere die Variabeln Rot, Blau und Gruen mit den jeweiligen PIN-Nummern. Erestze ? mit der richtigen Zahl (Du musst die PINs 9, 10 ,11 und GND wählen)
+  int Rot = ?;
   //TODO: Wiederhole dies für Blau und Gruen
   
 
 
 void setup() // Hier beginnt das Setup
 { 
-  //TODO: Rot, Blau und Gruen werden als Ausgang definiert. Ersetze YY mit OUTPUT oder INPUT
-  pinMode(Rot, YY); 
+  //TODO: Rot, Blau und Gruen werden als Ausgang definiert. Ersetze ? mit OUTPUT oder INPUT
+  pinMode(Rot, ?); 
   //TODO: definiere die weiteren Farben als Ausgang.
 
 
-  //TODO: Der Potentiometer wird als Eingang definiert. 
+  //TODO: definiere das RotaryPotentiometer als Eingang.
   
 } 
 
 void loop() // Hier beginnt das Hauptprogramm
 { 
-  //TODO: Definiert die Variable sensorWert und liest den Wert vom Potentiometer. Ersetze YY mit der richtigen Variabel.
-  sensorWert = analogRead(YY); 
+  //TODO: Definiert die Variable Sensorwert und liest den Wert vom Potentiometer. Ersetze ? mit der richtigen Variabel.
+  Sensorwert = analogRead(?); 
 
-  helligkeit = sensorwert/4; //1023 / 4 = 255 --> analogWrite geht nur bis 255 und Poti bis 1023 
+  Helligkeit = Sensorwert/4;       //1023 / 4 = 255 --> analogWrite geht nur bis 255 und RotaryPotentiometer bis 1023 
 
-  //Setze alle Farbkanäle der RGB-LED auf den gleichen Wert, um weißes Licht zu erzeugen
-  analogWrite(Rot, helligkeit); // Rot
+  //TODO: Setze alle Farbkanäle der RGB-LED auf den gleichen Wert, um weißes Licht zu erzeugen
+  analogWrite(Rot, Helligkeit); // Rot
   //TODO: Wiederhole dies mit den weiteren zwei Farben
 
 
