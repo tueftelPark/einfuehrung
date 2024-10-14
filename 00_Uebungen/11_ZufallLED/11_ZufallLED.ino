5adf545318d04fb64ff0fb073e1e23dd2a42d5e1
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2024-10-14 13:59:54 +0200

    Update 11_ZufallLED.ino

--- 00_Uebungen/11_ZufallLED/11_ZufallLED.ino
+++ 00_Uebungen/11_ZufallLED/11_ZufallLED.ino
@@ -1,11 +1,16 @@
-/* Befehle:
+/* 
+>> Was soll passieren? Was ist das Ziel der Übung? <<
+Hier wird ganz zufällig bestimmt wann deine LED eingeschaltet und augeschaltet wird.
+Mit dem Befehl random wird eine zufällig Zahl aus deinem Bereich ausgewählt.
+
+>> Befehle <<
 pinMode(PIN, OUTPUT oder INPUT); <- definiert den PIN(wo ist das Objekt angeschlossen), PIN mit der Zahl auf dem SensorKit ersetzen (ohne D), INPUT(Sensor/Signaleingang) OUTPUT(Aktor/Signalausgang)
 digitalWrite (PIN, HIGH oder LOW); <- schaltet etwas EIN (HIGH) oder AUS (LOW) - PIN mit der Zahl auf dem SensorKit ersetzen (ohne D), dann HIGH oder LOW
 delay(ZEIT); <- macht eine Pause mit der ZEIT in Millisekunden
 random(Startzahl, Schlusszahl); <- generiert eine zufällige Zahl zwischen Startzahl-Schlusszahl
 */
 
-  //TODO: Definiere die LED mit einer Variable.
+//TODO: Definiere die LED mit einer Variable.
 
 
 void setup() // Hier beginnt das Setup
@@ -32,5 +37,8 @@ void loop() // Hier beginnt das Hauptprogramm
   int ZeitAus = random(??, ??);
 
   //TODO: warte für das zweite zufällige Zeitintervall
+
+
+
   
 } // Hier endet ein Programmabschnitt
