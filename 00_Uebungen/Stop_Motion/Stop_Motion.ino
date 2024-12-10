97bc600b87d44647bb414315c6e3578bb8e26948
tüftelPark <leitung@tueftelpark-pilatus.ch>
2024-12-10 09:52:59 +0100

    v1

--- 00_Uebungen/Stop_Motion/Stop_Motion.ino
+++ 00_Uebungen/Stop_Motion/Stop_Motion.ino
@@ -15,15 +15,11 @@ void setup() {
 //
 void loop() {
 Buttonstatus = digitalRead(Button);
+// TODO: Rufe die Stop-Motion Bilder auf
 // Hier werden die unten definierten Funktionen aufgerufen
-// Wenn du den Buttuon drückst geht die Stop-Motion los
+// Wenn du den Button drückst geht die Stop-Motion los
   if (Buttonstatus == HIGH) 
   {
-  
-  Hund_6();
-  Hund_5();
-  Hund_4();
-  Hund_3();
   Hund_2();  
   Hund_1();
   Oled.clear();
@@ -31,7 +27,8 @@ Buttonstatus = digitalRead(Button);
 }
 
 
-// TODO: Hier definierst du die einelnen Bilder der Stop-Motion
+// TODO: Mache die einzelnen Stop-Motion Bilder
+// Hier definierst du die einelnen Bilder der Stop-Motion
 // Übernimm die vorgegebene Struktur!
 void Hund_1() {
   Oled.clear();
@@ -55,51 +52,8 @@ void Hund_2() {
   delay(Spielzeit);
 }
 
-void Hund_3() {
-  Oled.clear();
-  Oled.setCursor(0, 0);
-  Oled.println("  ^..^     /");
-  Oled.println("  /_/_____/ ");
-  Oled.println("     /   /  ");
-  Oled.println("    /   /   ");
-  Oled.refreshDisplay();
-  delay(Spielzeit);
-}
 
-void Hund_4() {
-  Oled.clear();
-  Oled.setCursor(0, 0);
-  Oled.println("   ^..^     /");
-  Oled.println("   /_/_____/ ");
-  Oled.println("      /   /  ");
-  Oled.println("     /   /   ");
-  Oled.refreshDisplay();
-  delay(Spielzeit);
-}
-
-void Hund_5() {
-  Oled.clear();
-  Oled.setCursor(0, 0);
-  Oled.println("    ^..^     /");
-  Oled.println("    /_/_____/ ");
-  Oled.println("       /   /  ");
-  Oled.println("      /   /   ");
-  Oled.refreshDisplay();
-  delay(Spielzeit);
-}
-void Hund_6() {
-  Oled.clear();
-  Oled.setCursor(0, 0);
-  Oled.println("     ^..^     /");
-  Oled.println("     /_/_____/ ");
-  Oled.println("        /   /  ");
-  Oled.println("       /   /   ");
-  Oled.refreshDisplay();
-  delay(Spielzeit);
-  
-}
-
-/* TEST
+/* Beispiele
 
 Oled.println("^..^      /");
 Oled.println("/_/_____/");
@@ -115,5 +69,22 @@ Oled.println("/_____/   U");
 Oled.println(" (\\_/)");
 Oled.println(" (o.o)");
 Oled.println(" (> <)");
+*/
+
+
+/*
+Vorlage if - else if  - else:
+
+if (Bedingung) {
+    Hund_1();
+  } else if (Bedingung2) {
+    Hund_2();
+  } else if (Bedingung3) {
+    ...
+  }
+  // Wenn keine Regel greift
+  else {
+    ...
+  }
 
 */
